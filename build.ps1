7788504d992021cf83c513f2a859784dd954c5ab
jdoubleu <hi@jdoubleu.de>
2022-08-04 17:32:11 +0200

    add vcpkg as submodule

--- build.ps1
+++ build.ps1
@@ -1,3 +1,7 @@
 # this script runs CMake and aims to build the project
+if (Test-Path -Path ".\vcpkg") {
+	$ENV:VCPKG_ROOT = ".\vcpkg"
+}
+
 cmake -B build/ -G "Visual Studio 17 2022" -DCMAKE_TOOLCHAIN_FILE="${ENV:VCPKG_ROOT}\scripts\buildsystems\vcpkg.cmake"
 cmake --build build/ --target ALL_BUILD --parallel
