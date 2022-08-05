313bad541ed777a880ddbe8635abf05efa8b88c0
jdoubleu <hi@jdoubleu.de>
2022-08-05 13:05:29 +0200

    move CMake configuration into CMakePresets.json

--- build.ps1
+++ build.ps1
@@ -3,5 +3,5 @@ if (Test-Path -Path ".\vcpkg") {
 	$ENV:VCPKG_ROOT = ".\vcpkg"
 }
 
-cmake -B build/ -G "Visual Studio 17 2022" -DCMAKE_TOOLCHAIN_FILE="${ENV:VCPKG_ROOT}\scripts\buildsystems\vcpkg.cmake" -DVCPKG_OVERLAY_TRIPLETS="$(pwd)/triplets/" -DVCPKG_TARGET_TRIPLET="x64-windows-static"
+cmake --preset default
 cmake --build build/ --target ALL_BUILD --parallel
